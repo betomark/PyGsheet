02160feeb798cd2039b3bc5ee963116ffd24669f
betomark <almark16@gmail.com>
2017-03-16 14:42:54 +0100

    adding setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pygsheet',
+      version='0.1',
+      description='wrapper for Google sheets API',
+      url='https://github.com/betomark/PyGsheet',
+      author='Alberto Marquez',
+      author_email='almark16@gmail.com',
+      license='GPL',
+      packages=[],
+      zip_safe=False)
