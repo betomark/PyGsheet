a343fc8aef573ab51c43e451134bc19928e97c2b
betomark <almark16@gmail.com>
2017-03-22 12:24:27 +0100

    updating version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='pygsheet',
 
-    version='0.1.7b1',
+    version='0.1.11b1',
 
     description='Wrapper for Google sheets API',
     long_description=long_description,
